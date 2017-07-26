0bfb2542cce901bd67a3e6568c79e354ea2dd72f
Diana Maria Silva Valencia <diana.silva.v94@gmail.com>
2017-07-26 09:06:12 -0500

    añadir a lista cosas por hacer

--- js/todolist.js
+++ js/todolist.js
@@ -1,11 +1,13 @@
-var listaFrutas = []; //array que contiene la lista de frutas
+var toDoList = []; 
 function agregar() {
-  var fruta = document.getElementById('fruta').value;//se obtiene lo que se ingresa en la caja de texto
-  listaFrutas.push(fruta);//se agrega al array
-  var resultado = '';//se crea una variable que contendra la lista
-  for(i in listaFrutas){
-    resultado += (Number(i)+1) + '. ' + listaFrutas[i].slice(0,1).toUpperCase() + listaFrutas[i].slice(1) + '<br>';
-  }//todo lo agregado, la primera letra es en mayuscula
-  document.getElementById('resultado').innerHTML = resultado; //se imprime la lista
-  fruta = '';//la caja de texto vuelve a estar en blanco
+  var list = document.getElementById("list").value;
+  toDoList.push(list);
+
+  var respuesta = "";
+  for(i in toDoList){
+    respuesta += (Number(i)+1) + ". " + toDoList[i].slice(0,1).toUpperCase() + toDoList[i].slice(1) + "<br>";
+  }
+  document.getElementById('respuesta').innerHTML = respuesta;
+  list = "";
 }
+console.log(toDoList);
