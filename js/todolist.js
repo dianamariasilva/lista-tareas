8db48b6c172382a9a868674ef600261dd4b9d62a
Diana Maria Silva Valencia <diana.silva.v94@gmail.com>
2017-07-26 15:49:32 -0500

    añadiendo elementos a lista

--- js/todolist.js
+++ js/todolist.js
@@ -1,13 +1,160 @@
-var toDoList = []; 
-function agregar() {
-  var list = document.getElementById("list").value;
-  toDoList.push(list);
-
-  var respuesta = "";
-  for(i in toDoList){
-    respuesta += (Number(i)+1) + ". " + toDoList[i].slice(0,1).toUpperCase() + toDoList[i].slice(1) + "<br>";
+ var array =[
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 6,
+    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 7,
+    "title": "illo expedita consequatur quia in",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 8,
+    "title": "quo adipisci enim quam ut ab",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 9,
+    "title": "molestiae perspiciatis ipsa",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 10,
+    "title": "illo est ratione doloremque quia maiores aut",
+    "completed": true
   }
-  document.getElementById('respuesta').innerHTML = respuesta;
-  list = "";
-}
-console.log(toDoList);
+];
+ var html = "";
+ for (var i =0; i<array.length;i++){
+  var dato_1 = array[i];
+  var s = "<div>" + 
+    "<h4>" + dato_1.userId + "-" +  dato_1.id + dato_1.title + dato_1.completed +"</h4>"+
+  "</div>";
+  html += s;
+ }
+document.getElementById("listaCreada").innerHTML=html;
+
+ var toDoList = []; 
+ function add() {
+    var list = document.getElementById("list").value;
+    toDoList.push(list);
+    var respuesta = "";
+    for(i in toDoList){
+        respuesta += "<div>" + "<h4>"+toDoList[i] +"</h4>"+"</div>";
+    }
+    document.getElementById('respuesta').innerHTML = respuesta;
+    list = "";
+  }
+  console.log(toDoList);
+
+  function tachar(){
+
+  }
+  function check(){
+
+  }
+
+ //var toDoList = []; 
+ // function agregar() {
+ //   var list = document.getElementById("list").value;
+ //   toDoList.push(list);
+
+ //   var respuesta = "";
+ //   for(i in toDoList){
+ //     respuesta += (Number(i)+1) + ". " + toDoList[i].slice(0,1).toUpperCase() + toDoList[i].slice(1) + "<br>";
+ //   }
+ //   document.getElementById('respuesta').innerHTML = respuesta;
+ //   list = "";
+ // }
+// console.log(toDoList);
+
+// var myNodelist = document.getElementsByTagName("LI");
+// var i;
+// for (i = 0; i < myNodelist.length; i++) {
+//   var span = document.createElement("SPAN");
+//   var txt = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   myNodelist[i].appendChild(span);
+// }
+
+// // Click on a close button to hide the current list item
+// var close = document.getElementsByClassName("close");
+// var i;
+// for (i = 0; i < close.length; i++) {
+//   close[i].onclick = function() {
+//     var div = this.parentElement;
+//     div.style.display = "none";
+//   }
+// }
+
+// // Add a "checked" symbol when clicking on a list item
+// var list = document.querySelector('ul');
+// list.addEventListener('click', function(ev) {
+//   if (ev.target.tagName === 'LI') {
+//     ev.target.classList.toggle('checked');
+//   }
+// }, false);
+
+// // Create a new list item when clicking on the "Add" button
+// function newElement() {
+//   var li = document.createElement("li");
+//   var inputValue = document.getElementById("myInput").value;
+//   var t = document.createTextNode(inputValue);
+//   li.appendChild(t);
+//   if (inputValue === '') {
+//     alert("You must write something!");
+//   } else {
+//     document.getElementById("a").appendChild(li);
+//   }
+//   document.getElementById("myInput").value = "";
+
+//   var span = document.createElement("SPAN");
+//   var txt = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   li.appendChild(span);
+
+//   for (i = 0; i < close.length; i++) {
+//     close[i].onclick = function() {
+//       var div = this.parentElement;
+//       div.style.display = "none";
+//     }
+//   }
+// }
