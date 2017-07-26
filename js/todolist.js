7a4f64f3aad6eec08465feb5e35e4aa4b2d948e6
Diana Maria Silva Valencia <diana.silva.v94@gmail.com>
2017-07-26 17:09:06 -0500

    poner x en frases cuando se marca checkbox

--- js/todolist.js
+++ js/todolist.js
@@ -63,8 +63,8 @@
  var html = "";
  for (var i =0; i<array.length;i++){
   var dato_1 = array[i];
-  var s = "<div>" + 
-    "<h4>" + dato_1.userId + "-" +  dato_1.id + dato_1.title + dato_1.completed +"</h4>"+
+  var s = "<div>" +  "<label>"+"<input type='checkbox'>"+
+    "<td>" + "<strong>"+ dato_1.userId + "-" +  dato_1.id + dato_1.title + dato_1.completed + "</strong>"+ "</td>"+ "</input>"+"</label>"+
   "</div>";
   html += s;
  }
@@ -76,7 +76,7 @@ document.getElementById("listaCreada").innerHTML=html;
     toDoList.push(list);
     var respuesta = "";
     for(i in toDoList){
-        respuesta += "<div>" + "<h4>"+toDoList[i] +"</h4>"+"</div>";
+        respuesta += "<div>" + "<label>"+"<input type='checkbox' name='check4'>"+"<td>"+ "<strong>"+toDoList[i] +"</strong>"+"</td>"+"</input>"+"</label>"+ "</div>";
     }
     document.getElementById('respuesta').innerHTML = respuesta;
     list = "";
@@ -84,77 +84,14 @@ document.getElementById("listaCreada").innerHTML=html;
   console.log(toDoList);
 
   function tachar(){
-
+      for(i in toDoList){
+        respuesta += "<del>" + "<td>"+toDoList[i] +"</td>"+"</del>";
+    	document.getElementById('respuesta_tachada').innerHTML = respuesta;
+    	list = "";
   }
+}
   function check(){
 
   }
 
- //var toDoList = []; 
- // function agregar() {
- //   var list = document.getElementById("list").value;
- //   toDoList.push(list);
 
- //   var respuesta = "";
- //   for(i in toDoList){
- //     respuesta += (Number(i)+1) + ". " + toDoList[i].slice(0,1).toUpperCase() + toDoList[i].slice(1) + "<br>";
- //   }
- //   document.getElementById('respuesta').innerHTML = respuesta;
- //   list = "";
- // }
-// console.log(toDoList);
-
-// var myNodelist = document.getElementsByTagName("LI");
-// var i;
-// for (i = 0; i < myNodelist.length; i++) {
-//   var span = document.createElement("SPAN");
-//   var txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   myNodelist[i].appendChild(span);
-// }
-
-// // Click on a close button to hide the current list item
-// var close = document.getElementsByClassName("close");
-// var i;
-// for (i = 0; i < close.length; i++) {
-//   close[i].onclick = function() {
-//     var div = this.parentElement;
-//     div.style.display = "none";
-//   }
-// }
-
-// // Add a "checked" symbol when clicking on a list item
-// var list = document.querySelector('ul');
-// list.addEventListener('click', function(ev) {
-//   if (ev.target.tagName === 'LI') {
-//     ev.target.classList.toggle('checked');
-//   }
-// }, false);
-
-// // Create a new list item when clicking on the "Add" button
-// function newElement() {
-//   var li = document.createElement("li");
-//   var inputValue = document.getElementById("myInput").value;
-//   var t = document.createTextNode(inputValue);
-//   li.appendChild(t);
-//   if (inputValue === '') {
-//     alert("You must write something!");
-//   } else {
-//     document.getElementById("a").appendChild(li);
-//   }
-//   document.getElementById("myInput").value = "";
-
-//   var span = document.createElement("SPAN");
-//   var txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   li.appendChild(span);
-
-//   for (i = 0; i < close.length; i++) {
-//     close[i].onclick = function() {
-//       var div = this.parentElement;
-//       div.style.display = "none";
-//     }
-//   }
-// }
